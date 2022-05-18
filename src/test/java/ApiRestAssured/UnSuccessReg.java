a05efba56f41147b9f96deb50b191589e478c19c
VasilyevVA <vladislavovix@live.com>
2022-05-18 14:54:38 +0700

    Initial commit

---
+++ src/test/java/ApiRestAssured/UnSuccessReg.java
@@ -0,0 +1,13 @@
+package ApiRestAssured;
+
+public class UnSuccessReg {
+    private String error;
+
+    public UnSuccessReg(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
